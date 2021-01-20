ff84de52e5eece1c36b2923f2ded64a8c8b69e62
Pedrolustosa <pedroeternalss@gmail.com>
2021-01-20 19:38:31 -0300

    Adaptação para MySQL e 1° Migration

---
+++ SalesWeb/Data/SalesWebContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Models;
+
+    public class SalesWebContext : DbContext
+    {
+        public SalesWebContext (DbContextOptions<SalesWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWeb.Models.Department> Department { get; set; }
+    }
